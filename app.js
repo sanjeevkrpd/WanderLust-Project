f1dd4b07064eca3fd4030c59774c3354168b4db7
Sanjeev kumar <Sanjeevkrpd11@gmail.com>
2024-02-26 10:31:37 +0530

    some styling

--- app.js
+++ app.js
@@ -92,8 +92,12 @@ app.use("/listings",listingsRouter);
 app.use("/listings/:id/review" , reviewRouter);
 app.use("/",userRouter);
 
+app.get("/",(req,res)=>{
+    res.redirect("/listings");
+})
 
 app.all("*", (req, res, next) => {
+
     next(new ExpressError(404, "PAGE NOT FOUND ! :("));
 });
 
